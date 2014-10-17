6fb4838ae1df7a0048be52c429d6dbaad5b001f4
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-10-17 12:24:34 -0700

    Refactored about:news into multiple views

---
+++ data/js/newsApp.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let newsApp = angular.module("newsApp", ["ngRoute", "newsAppControllers"]);
+
+newsApp.controller('MainController', function($scope, $route, $routeParams, $location) {
+  $scope.dataUrl = self.options.dataUrl;
+  $scope.state = {
+    score: true,
+    site: false,
+  };
+
+  function setState(what) {
+    Object.keys($scope.state).forEach(view => {
+      $scope.state[view] = false;
+    });
+    $scope.state[what] = true;
+  };
+
+  $scope.navigate = function(view) {
+    setState(view);
+  };
+});
+
+newsApp.config(function($routeProvider, $locationProvider, $sceDelegateProvider, $compileProvider) {
+   $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    self.options.dataUrl + "/**",
+    'docScoresView.html',
+   ]);
+
+  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|resource):/);
+});
+
+// Low-level data injections
+self.port.on("style", function(file) {
+  let link = document.createElement("link");
+  link.setAttribute("href", file);
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  document.head.appendChild(link);
+});
+
+self.port.on("import", function(data) {
+  $('head').append(data);
+});
+
+self.port.on("bootstrap", function() {
+  angular.bootstrap(document, ['newsApp']);
+});
